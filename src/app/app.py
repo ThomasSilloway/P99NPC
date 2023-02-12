8fdba066ffaa5d2bce37019e228d028c04316380
ThomasSilloway <tcsillow@gmail.com>
2023-02-11 21:10:08 -0500

    Updated to run the /who command every 3 seconds
    But only if the window is active! Which is pretty sweet, that way it doesn't mess up all of your other programs

--- src/app/app.py
+++ src/app/app.py
@@ -22,15 +22,21 @@ class App:
         # win.send("1")
         # win.move(x=200, y=300, width=500, height=800)
         ahk.mouse_move(x=800, y=400, speed=5)
-        ahk.send("i")
+        # Run who command
+        while True:
+            if win.active:
+                # Run who command to make sure text input is cleared out
+                ahk.send("/{enter}")
+                ahk.send("/who all \"Legacy of Ik\"{enter}")
+                time.sleep(3)
         # ahk.type("1")
         # ahk.key_press("i")
         # ahk.send_input("i")
-        ahk.key_down("w")
-        time.sleep(0.5)
-        ahk.key_up("w")
+        # ahk.key_down("w")
+        # time.sleep(0.5)
+        # ahk.key_up("w")
 
-        ahk.show_tooltip("This is a friendly tooltip? ", second=2, x=800, y=400)  # ToolTip
+        # ahk.show_tooltip("This is a friendly tooltip? ", second=2, x=800, y=400)  # ToolTip
         # for window in ahk.windows():
         #     print(window.title)
         #     print(window.process)
